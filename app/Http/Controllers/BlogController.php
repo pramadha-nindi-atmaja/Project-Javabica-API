7d9571ebc01b30a2b4894ae55afd52b2169f8a7e
Pramadha <pramslabs@gmail.com>
2025-11-13 10:26:32 +0700

    Improve BlogController for validation consistency and cleaner structure

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -14,143 +14,197 @@ use App\Services\S3uploaderServices;
 
 class BlogController extends BaseController
 {
-    private $blogInterface;
-    private $s3uploaderService;
+    private BlogInterface $blogInterface;
+    private S3uploaderServices $s3uploaderService;
 
-    
-    public function __construct(BlogInterface $blogInterface,S3uploaderServices $s3uploaderService)
+    public function __construct(BlogInterface $blogInterface, S3uploaderServices $s3uploaderService)
     {
-        $this->blogInterface            = $blogInterface;
-        $this->s3uploaderService            = $s3uploaderService;
-    
+        $this->blogInterface = $blogInterface;
+        $this->s3uploaderService = $s3uploaderService;
     }
-    public function show(GetBlogRequestValidation $request) {
 
-        $selectedColumn = array('*');
+    /**
+     * Display a list of blogs or a specific blog.
+     */
+    public function show(GetBlogRequestValidation $request)
+    {
+        $selectedColumn = ['*'];
+        $getBlog = $this->blogInterface->show($request, $selectedColumn);
 
-        $getBlog = $this->blogInterface->show($request,$selectedColumn);
-        
-        if($getBlog['queryStatus']) {
-            
-            return $this->handleResponse( $getBlog['queryResponse'],'get Blog success',$request->all(),str_replace('/','.',$request->path()),201);
-        }
-
-        $data  = array([
-            'field' =>'show-blog',
-            'message'=> 'error when show Blog '
-        ]);
-
-        return   $this->handleError( $data,$getBlog['queryMessage'],$request->all(),str_replace('/','.',$request->path()),422);
-    }
-    public function create(CreateBlogRequest $request) { 
-
-        if( $request->file('cover_upload') &&  $request->file('cover_upload') != null) {
-            $this->uploaderValidation($request);
-            
-
-            $fileData = $this->s3uploaderService->uploads3Storage($request->file('cover_upload'),'dynamic');
-
-            $request->merge([
-                'cover' => $fileData['arrayResponse']['filePath']
-            ]);
-
-        }
-        else {
-            $request->merge([
-                'cover' =>$request->cover_upload
-            ]);
-        }
-
-        $insert = $this->blogInterface->store($request->all(),'show_all');
-
-        if($insert['queryStatus']) {
-
-            return $this->handleResponse( $insert['queryResponse'],'Insert blog  success',$request->all(),str_replace('/','.',$request->path()),201);
-        }
-        else {
-
-            $data  = array([
-                'field' =>'create-blog',
-                'message'=> 'blog create fail'
-            ]);
-
-            return   $this->handleError($data,$insert['queryMessage'],$request->all(),str_replace('/','.',$request->path()),422);
-        }
-
-    }
-    public function update(UpdateBlogRequest $request) { //done
-        
-        if( $request->file('cover_upload')&&  $request->file('cover_upload')  != null) {
-            
-            $this->uploaderValidation($request);
-
-            $datas = Blog::find($request->id);
-            $getimage = $datas->cover;
-            
-            $fileData = $this->s3uploaderService->uploads3Storage($request->file('cover_upload'),'dynamic',$getimage);
-
-            $request->merge([
-                'cover' => $fileData['arrayResponse']['filePath']
-            ]);
-
-            
-        } else {
-
-            $request->merge([
-                'cover' =>$request->cover_upload
-            ]);
-        }
-
-        $update = $this->blogInterface->update($request->id,$request->except(['id']),'show_all');
-
-        if($update['queryStatus']) {
-
-            $data  = array(
-                'field' =>'update-blog',
-                'message'=> 'blog successfuly updated'
+        if ($getBlog['queryStatus']) {
+            return $this->handleResponse(
+                $getBlog['queryResponse'],
+                'Get blog success',
+                $request->validated(),
+                str_replace('/', '.', $request->path()),
+                200
             );
-            
-            return $this->handleResponse( $update['queryResponse'],'update blog success',$request->all(),str_replace('/','.',$request->path()),201);
-        }
-        else {
-
-            $data  = array([
-                'field' =>'update-blog',
-                'message'=> 'blog update fail'
-            ]);
-
-            return   $this->handleError($data,$update['queryMessage'],$request->all(),str_replace('/','.',$request->path()),422);
         }
 
+        $data = [
+            [
+                'field' => 'show-blog',
+                'message' => 'Error when showing blog',
+            ],
+        ];
+
+        return $this->handleError(
+            $data,
+            $getBlog['queryMessage'],
+            $request->validated(),
+            str_replace('/', '.', $request->path()),
+            422
+        );
     }
 
-    public function delete(DestroyBlogRequest $request) {
-        $destroy =   $this->blogInterface->destroy($request->by_id);
+    /**
+     * Create a new blog entry.
+     */
+    public function create(CreateBlogRequest $request)
+    {
+        if ($request->hasFile('cover_upload')) {
+            $this->validateUploader($request);
+            $fileData = $this->s3uploaderService->uploads3Storage($request->file('cover_upload'), 'dynamic');
 
-        if($destroy['queryStatus']) {
-
-            return $this->handleResponse( $destroy['queryResponse'],'delete blog success',$request->all(),str_replace('/','.',$request->path()),201);
-        }
-        else {
-
-            $data  = array([
-                'field' =>'destroy-blog',
-                'message'=> 'blog delete fail'
+            $request->merge([
+                'cover' => $fileData['arrayResponse']['filePath'],
             ]);
-
-            return   $this->handleError($data,$destroy['queryMessage'],$request->all(),str_replace('/','.',$request->path()),422);
+        } else {
+            $request->merge([
+                'cover' => $request->cover_upload,
+            ]);
         }
+
+        $insert = $this->blogInterface->store($request->validated(), 'show_all');
+
+        if ($insert['queryStatus']) {
+            return $this->handleResponse(
+                $insert['queryResponse'],
+                'Blog created successfully',
+                $request->validated(),
+                str_replace('/', '.', $request->path()),
+                201
+            );
+        }
+
+        $data = [
+            [
+                'field' => 'create-blog',
+                'message' => 'Blog creation failed',
+            ],
+        ];
+
+        return $this->handleError(
+            $data,
+            $insert['queryMessage'],
+            $request->validated(),
+            str_replace('/', '.', $request->path()),
+            422
+        );
     }
 
-    private function uploaderValidation($request) {
-        $validator = Validator::make($request->only(['cover']), [
-            'cover' =>  config('formValidation.image_upload'),       
+    /**
+     * Update an existing blog.
+     */
+    public function update(UpdateBlogRequest $request)
+    {
+        if ($request->hasFile('cover_upload')) {
+            $this->validateUploader($request);
+
+            $existingBlog = Blog::find($request->id);
+            $existingCover = $existingBlog?->cover;
+
+            $fileData = $this->s3uploaderService->uploads3Storage(
+                $request->file('cover_upload'),
+                'dynamic',
+                $existingCover
+            );
+
+            $request->merge([
+                'cover' => $fileData['arrayResponse']['filePath'],
+            ]);
+        } else {
+            $request->merge([
+                'cover' => $request->cover_upload,
+            ]);
+        }
+
+        $update = $this->blogInterface->update(
+            $request->id,
+            $request->except(['id']),
+            'show_all'
+        );
+
+        if ($update['queryStatus']) {
+            return $this->handleResponse(
+                $update['queryResponse'],
+                'Blog updated successfully',
+                $request->validated(),
+                str_replace('/', '.', $request->path()),
+                200
+            );
+        }
+
+        $data = [
+            [
+                'field' => 'update-blog',
+                'message' => 'Blog update failed',
+            ],
+        ];
+
+        return $this->handleError(
+            $data,
+            $update['queryMessage'],
+            $request->validated(),
+            str_replace('/', '.', $request->path()),
+            422
+        );
+    }
+
+    /**
+     * Delete a blog entry.
+     */
+    public function delete(DestroyBlogRequest $request)
+    {
+        $destroy = $this->blogInterface->destroy($request->by_id);
+
+        if ($destroy['queryStatus']) {
+            return $this->handleResponse(
+                $destroy['queryResponse'],
+                'Blog deleted successfully',
+                $request->validated(),
+                str_replace('/', '.', $request->path()),
+                200
+            );
+        }
+
+        $data = [
+            [
+                'field' => 'destroy-blog',
+                'message' => 'Blog deletion failed',
+            ],
+        ];
+
+        return $this->handleError(
+            $data,
+            $destroy['queryMessage'],
+            $request->validated(),
+            str_replace('/', '.', $request->path()),
+            422
+        );
+    }
+
+    /**
+     * Validate the uploaded file for cover image.
+     */
+    private function validateUploader($request): void
+    {
+        $validator = Validator::make($request->only(['cover_upload']), [
+            'cover_upload' => config('formValidation.image_upload'),
         ]);
 
-        if ($validator->fails()) { 
-         
-            throw ValidationException::withMessages( $validator->errors()->all());
+        if ($validator->fails()) {
+            throw ValidationException::withMessages($validator->errors()->all());
         }
     }
-
 }
