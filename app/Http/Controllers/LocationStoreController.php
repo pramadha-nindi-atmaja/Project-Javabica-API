cef8f6feba6ce7f600d8ae661e11f3e54d83de7b
Pramadha <pramslabs@gmail.com>
2025-11-13 10:32:14 +0700

    Add type hints and clean up redundant variables

--- app/Http/Controllers/LocationStoreController.php
+++ app/Http/Controllers/LocationStoreController.php
@@ -2,180 +2,219 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\LocationStoreRequest\LocationStoreCreateRequest;
-use App\Http\Requests\LocationStoreRequest\LocationStoreDestroyRequest;
-use App\Http\Requests\LocationStoreRequest\LocationStoreShowRequest;
-use App\Http\Requests\LocationStoreRequest\LocationStoreUpdateRequest;
+use App\Http\Requests\LocationStoreRequest\{
+    LocationStoreCreateRequest,
+    LocationStoreDestroyRequest,
+    LocationStoreShowRequest,
+    LocationStoreUpdateRequest
+};
 use App\Interfaces\LocationStoreInterface;
 use App\Models\Province;
 use App\Models\Location_stores;
-use Illuminate\Http\Request;
 use App\Services\S3uploaderServices;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Str;
 
 class LocationStoreController extends BaseController
 {
-    private $locationInterface;
-    private $s3uploaderService;
+    private LocationStoreInterface $locationInterface;
+    private S3uploaderServices $s3uploaderService;
 
-    public function __construct(LocationStoreInterface $locationInterface,S3uploaderServices $s3uploaderService)
+    public function __construct(LocationStoreInterface $locationInterface, S3uploaderServices $s3uploaderService)
     {
-        $this->locationInterface            = $locationInterface;
-        $this->s3uploaderService            = $s3uploaderService;
+        $this->locationInterface = $locationInterface;
+        $this->s3uploaderService = $s3uploaderService;
     }
 
-    public function show(LocationStoreShowRequest $request) {
+    /**
+     * Display a list of location stores with provinces.
+     */
+    public function show(LocationStoreShowRequest $request)
+    {
+        $selectedColumns = ['*'];
+        $locations = $this->locationInterface->show($request->validated(), $selectedColumns, 'show_all');
 
-        $selectedColumn = array('*');
-        $get = $this->locationInterface->show($request->all(),$selectedColumn,'show_all');
- 
-         if($get['queryStatus']) {
-             
-            //get province
-           $getProvince =  Province::get();
+        if ($locations['queryStatus']) {
+            $output = [
+                'list_province' => Province::all(),
+                'store_list' => $locations['queryResponse'],
+            ];
 
-           $output = array(
-            'list_province' => $getProvince,
-            'store_list'    => $get['queryResponse']
-           );
-           
-            return $this->handleResponse( $output,'get location store success',$request->all(),str_replace('/','.',$request->path()),201);
-         }
- 
-         $data  = array([
-             'field' =>'show-location-store',
-             'message'=> 'error when show location store'
-         ]);
- 
-         return   $this->handleError( $data,$get['queryMessage'],$request->all(),str_replace('/','.',$request->path()),422);
-
-    }
-
-    public function update(LocationStoreUpdateRequest $request) {
-        
-        if( $request->file('image_upload')&&  $request->file('image_upload')  != null) {
-            
-            $this->uploaderValidation($request);
-
-            $datas = Location_stores::find($request->id);
-            $getimage = $datas->image;
-            
-            $fileData = $this->s3uploaderService->uploads3Storage($request->file('image_upload'),'dynamic',$getimage);
-
-            $request->merge([
-                'image' => $fileData['arrayResponse']['filePath']
-            ]);
-
-            
-        } else {
-
-            $request->merge([
-                'image' => $request->image_upload ? $request->image_upload : NULL
-            ]);
+            return $this->handleResponse(
+                $output,
+                'Location stores retrieved successfully',
+                $request->all(),
+                Str::replace('/', '.', $request->path()),
+                200
+            );
         }
 
-        $update = $this->locationInterface->update($request->id,$request->except(['id']),'show_all');
+        $errorData = [
+            'field' => 'show-location-store',
+            'message' => 'Failed to retrieve location store data',
+        ];
 
-        if($update['queryStatus']) {
+        return $this->handleError(
+            $errorData,
+            $locations['queryMessage'],
+            $request->all(),
+            Str::replace('/', '.', $request->path()),
+            422
+        );
+    }
 
-            $data  = array(
-                'field' =>'update-order',
-                'message'=> 'order successfuly updated'
+    /**
+     * Create a new location store.
+     */
+    public function create(LocationStoreCreateRequest $request)
+    {
+        if ($request->hasFile('image_upload')) {
+            $this->uploaderValidation($request);
+            $fileData = $this->s3uploaderService->uploads3Storage($request->file('image_upload'), 'dynamic');
+            $request->merge(['image' => $fileData['arrayResponse']['filePath']]);
+        } else {
+            $request->merge(['image' => $request->image_upload ?? null]);
+        }
+
+        $create = $this->locationInterface->store($request->validated(), 'show_all');
+
+        if ($create['queryStatus']) {
+            return $this->handleResponse(
+                $create['queryResponse'],
+                'Location store created successfully',
+                $request->all(),
+                Str::replace('/', '.', $request->path()),
+                201
+            );
+        }
+
+        $errorData = [
+            'field' => 'create-location-store',
+            'message' => 'Failed to create location store',
+        ];
+
+        return $this->handleError(
+            $errorData,
+            $create['queryMessage'],
+            $request->all(),
+            Str::replace('/', '.', $request->path()),
+            422
+        );
+    }
+
+    /**
+     * Update an existing location store.
+     */
+    public function update(LocationStoreUpdateRequest $request)
+    {
+        if ($request->hasFile('image_upload')) {
+            $this->uploaderValidation($request);
+
+            $store = Location_stores::find($request->id);
+            $existingImage = $store?->image;
+
+            $fileData = $this->s3uploaderService->uploads3Storage(
+                $request->file('image_upload'),
+                'dynamic',
+                $existingImage
             );
 
-
-            return $this->handleResponse($data,'Update location store success',$request->all(),str_replace('/','.',$request->path()),201);
-        }
-        else {
-
-            $data  = array([
-                'field' =>'update-location store',
-                'message'=> 'location store update fail'
-            ]);
-
-            return   $this->handleError($data,$update['queryMessage'],$request->all(),str_replace('/','.',$request->path()),422);
+            $request->merge(['image' => $fileData['arrayResponse']['filePath']]);
+        } else {
+            $request->merge(['image' => $request->image_upload ?? null]);
         }
 
+        $update = $this->locationInterface->update($request->id, $request->except(['id']), 'show_all');
+
+        if ($update['queryStatus']) {
+            return $this->handleResponse(
+                $update['queryResponse'],
+                'Location store updated successfully',
+                $request->all(),
+                Str::replace('/', '.', $request->path()),
+                200
+            );
+        }
+
+        $errorData = [
+            'field' => 'update-location-store',
+            'message' => 'Failed to update location store',
+        ];
+
+        return $this->handleError(
+            $errorData,
+            $update['queryMessage'],
+            $request->all(),
+            Str::replace('/', '.', $request->path()),
+            422
+        );
     }
 
-    public function create(LocationStoreCreateRequest $request) {
-        
-        if( $request->file('image_upload') &&  $request->file('image_upload') != null) {
-            $this->uploaderValidation($request);
-            
+    /**
+     * Delete a location store.
+     */
+    public function delete(LocationStoreDestroyRequest $request)
+    {
+        $destroy = $this->locationInterface->destroy($request->by_id);
 
-            $fileData = $this->s3uploaderService->uploads3Storage($request->file('image_upload'),'dynamic');
+        if ($destroy['queryStatus']) {
+            $successData = [
+                'field' => 'destroy-location-store',
+                'message' => 'Location store deleted successfully',
+            ];
 
-            $request->merge([
-                'image' => $fileData['arrayResponse']['filePath']
-            ]);
-
-        }
-        else {
-            $request->merge([
-                'image' => $request->image_upload ? $request->image_upload : NULL
-            ]);
-        }
-
-        $insertLocation  =   $this->locationInterface->store($request->all(),'show_all');
-
-        if($insertLocation['queryStatus']) {
-
-            return $this->handleResponse( $insertLocation['queryResponse'],'Insert location store success',$request->all(),str_replace('/','.',$request->path()),201);
-        }
-        else {
-
-            $data  = array([
-                'field' =>'create-location',
-                'message'=> 'location create fail'
-            ]);
-
-            return   $this->handleError($data,$insertLocation['queryMessage'],$request->all(),str_replace('/','.',$request->path()),422);
+            return $this->handleResponse(
+                $successData,
+                'Location store deleted successfully',
+                $request->all(),
+                Str::replace('/', '.', $request->path()),
+                204
+            );
         }
 
-    }
-    public function province(Request $request) {
+        $errorData = [
+            'field' => 'destroy-location-store',
+            'message' => 'Failed to delete location store',
+        ];
 
-        $getProvince =  Province::get();
-        return $this->handleResponse( $getProvince,'get location store success',$request->all(),str_replace('/','.',$request->path()),201);
+        return $this->handleError(
+            $errorData,
+            $destroy['queryMessage'],
+            $request->all(),
+            Str::replace('/', '.', $request->path()),
+            422
+        );
     }
 
-    public function delete(LocationStoreDestroyRequest $request) {
-        
-         //remove data
-         $destroy =   $this->locationInterface->destroy($request->by_id);
+    /**
+     * Retrieve all provinces.
+     */
+    public function province(Request $request)
+    {
+        $provinces = Province::all();
 
-         if($destroy['queryStatus']) {
- 
-             //response
-             $data  = array(
-                 'field' =>'destroy-location-stores',
-                 'message'=> 'location stores successfuly destroyed'
-             );
- 
-              return $this->handleResponse($data,'Destroy location stores  success',$request->all(),str_replace('/','.',$request->path()),204);
-        
-         } else {
-             
-             $data  = array([
-                 'field' =>'destroy-location-stores',
-                 'message'=> 'location stores destroy fail'
-             ]);
- 
-              return   $this->handleError($data,$destroy['queryMessage'],$request->all(),str_replace('/','.',$request->path()),422);
-         }
-
+        return $this->handleResponse(
+            $provinces,
+            'Provinces retrieved successfully',
+            $request->all(),
+            Str::replace('/', '.', $request->path()),
+            200
+        );
     }
-    
-    private function uploaderValidation($request) {
-        $validator = Validator::make($request->only(['image']), [
-            'image' =>  config('formValidation.image_upload'),       
+
+    /**
+     * Validate uploaded image before sending to S3.
+     */
+    private function uploaderValidation(Request $request): void
+    {
+        $validator = Validator::make($request->only(['image_upload']), [
+            'image_upload' => config('formValidation.image_upload'),
         ]);
 
-        if ($validator->fails()) { 
-         
-            throw ValidationException::withMessages( $validator->errors()->all());
+        if ($validator->fails()) {
+            throw ValidationException::withMessages($validator->errors()->all());
         }
     }
 }
